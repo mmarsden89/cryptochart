03c4d07ed0f20893ee458539ee0ac45a9d110a50
mmarsden89 <mattmarsdendev@gmail.com>
2021-08-20 15:37:04 -0400

    start primitive work on components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./App.scss";
+import Navbar from "./Components/Navbar/Navbar.js";
+import axios from "axios";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="component-container">
+        <div
+          style={{
+            backgroundColor: "white",
+            width: "500px",
+            height: "500px",
+            border: "1px solid #d1cece",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <div className="navbar-title">cryptoChart</div>
+    </div>
+  );
+};
+
+export default Navbar;
