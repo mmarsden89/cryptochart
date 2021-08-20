d0ae83fade93ff720fb107011a370f18dcbb42f9
mmarsden89 <mattmarsdendev@gmail.com>
2021-08-20 16:04:03 -0400

    add testing data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import "./App.scss";
 import Navbar from "./Components/Navbar/Navbar.js";
 import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(async () => {
+    const call = await axios(
+      "https://min-api.cryptocompare.com/data/histoday?fsym=BTC&tsym=USD&limit=10"
+    );
+    console.log(call.data);
+  });
+
   return (
     <div className="App">
       <Navbar />
