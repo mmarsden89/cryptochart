5c9f4643758a01145feb9eb8257badd22b456e5a
mmarsden89 <mattmarsdendev@gmail.com>
2021-09-20 12:45:07 -0400

    Add News component

---
+++ src/Components/News.js
@@ -0,0 +1,13 @@
+import "./News.scss";
+
+const News = (props) => {
+  const { url, imageurl, title } = props.data;
+  return (
+    <a href={url} target="_blank" className="news-item-container" title={title}>
+      <img src={imageurl} className="news-image" />
+      <p className="news-title">{title.replace(/^(.{85}[^\s]*).*/, "$1...")}</p>
+    </a>
+  );
+};
+
+export default News;
