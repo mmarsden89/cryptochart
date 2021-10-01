91ab73235741164134e3f41c562d0ae3571c685f
mmarsden89 <mattmarsdendev@gmail.com>
2021-10-01 15:18:43 -0400

    impliment 404 page component

---
+++ src/Components/FourZeroFour/FourZeroFour.js
@@ -0,0 +1,23 @@
+const FourZeroFour = () => {
+  return (
+    <div
+      style={{
+        height: "calc(100vh - 38px)",
+        width: "calc(100% + 14px)",
+        marginLeft: "-14px",
+        marginTop: "-68px",
+      }}
+    >
+      <iframe
+        src="https://mmarsden89.github.io/404/"
+        title="404"
+        style={{
+          height: "100%",
+          width: "100%",
+        }}
+      />
+    </div>
+  );
+};
+
+export default FourZeroFour;
