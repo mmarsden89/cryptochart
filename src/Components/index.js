6bfa283a7e078353e4adda20818f815dbc26f35c
mmarsden89 <mattmarsdendev@gmail.com>
2021-12-10 13:57:23 -0500

    add index to component heirarchy

---
+++ src/Components/index.js
@@ -0,0 +1,7 @@
+import Navbar from "./Navbar/Navbar.js";
+import ErrorMessage from "./ErrorMessage/ErrorMessage.js";
+import ChartWrapper from "./Charts/ChartWrapper.js";
+import NewsWrapper from "./News/NewsWrapper.js";
+import FourZeroFour from "./FourZeroFour/FourZeroFour.js";
+
+export { Navbar, ErrorMessage, ChartWrapper, NewsWrapper, FourZeroFour };
